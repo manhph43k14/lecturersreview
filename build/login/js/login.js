22356de9ec4fc78681544b868a56cb5e8480fc34
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-03-12 13:24:31 +0700

    Update login.js

--- build/login/js/login.js
+++ build/login/js/login.js
@@ -1,164 +1,18 @@
-<<<<<<< HEAD
-// sign in 
-function toggleSignIn() {
-    if (firebase.auth().currentUser) {
-        // [START signout]
-        // window.location = "../home/index.html"
-        firebase.auth().signOut();
-        // [END signout]
-    } else {
-        var email = document.getElementById('email_login').value;
-        var password = document.getElementById('password_login').value;
-        if (email.length < 4) {
-            alert('Please enter an email address.');
-            return;
-        }
-        if (password.length < 4) {
-            alert('Please enter a password.');
-            return;
-        }
-        // Sign in with email and pass.
-        // [START authwithemail]
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // [START_EXCLUDE]
-            if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
-            } else {
-                alert(errorMessage);
-            }
-            console.log(error);
-            // document.getElementById('btnLogin').disabled = false;
-            // [END_EXCLUDE]
-        });
-        // [END authwithemail]
-    }
-    // document.getElementById('btnLogin').disabled = true;
+// Sign in
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+});
+// Sign up
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+});
+
+function login() {
+    alert("Working!")
 }
-
-/**
- * Handles the sign up button press.
- */
-function handleSignUp() {
-    var email = document.getElementById('email_signup').value;
-    var password = document.getElementById('password_signup').value;
-    if (email.length < 4) {
-        alert('Please enter an email address.');
-        return;
-    }
-    if (password.length < 4) {
-        alert('Please enter a password.');
-        return;
-    }
-    // Create user with email and pass.
-    // [START createwithemail]
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode == 'auth/weak-password') {
-            alert('The password is too weak.');
-        } else {
-            alert(errorMessage);
-        }
-        console.log(error);
-        // [END_EXCLUDE]
-    });
-    // [END createwithemail]
-}
-
-/**
- * Sends an email verification to the user.
- */
-function sendEmailVerification() {
-    // [START sendemailverification]
-    firebase.auth().currentUser.sendEmailVerification().then(function() {
-        // Email Verification sent!
-        // [START_EXCLUDE]
-        alert('Email Verification Sent!');
-        // [END_EXCLUDE]
-    });
-    // [END sendemailverification]
-}
-
-function sendPasswordReset() {
-    var email = document.getElementById('email').value;
-    // [START sendpasswordemail]
-    firebase.auth().sendPasswordResetEmail(email).then(function() {
-        // Password Reset Email Sent!
-        // [START_EXCLUDE]
-        alert('Password Reset Email Sent!');
-        // [END_EXCLUDE]
-    }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode == 'auth/invalid-email') {
-            alert(errorMessage);
-        } else if (errorCode == 'auth/user-not-found') {
-            alert(errorMessage);
-        }
-        console.log(error);
-        // [END_EXCLUDE]
-    });
-    // [END sendpasswordemail];
-}
-
-/**
- * initApp handles setting up UI event listeners and registering Firebase auth listeners:
- *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
- *    out, and that is where we update the UI.
- */
-function initApp() {
-    // Listening for auth state changes.
-    // [START authstatelistener]
-    firebase.auth().onAuthStateChanged(function(user) {
-        // [START_EXCLUDE silent]
-        // document.getElementById('btnLogin').disabled = true;
-        // [END_EXCLUDE]
-        if (user) {
-            // User is signed in.
-            var displayName = user.displayName;
-            var email = user.email;
-            var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            var providerData = user.providerData;
-            // [START_EXCLUDE]
-
-            // document.getElementById('btnLogin').textContent = 'Sign out';
-
-            if (!emailVerified) {
-                document.getElementById('quickstart-verify-email').disabled = false;
-            }
-            // [END_EXCLUDE]
-        } else {
-            // User is signed out.
-            // [START_EXCLUDE]
-
-            // document.getElementById('btnLogin').textContent = 'Sign in';
-
-            // [END_EXCLUDE]
-        }
-        // [START_EXCLUDE silent]
-        // document.getElementById('btnLogin').disabled = false;
-        // [END_EXCLUDE]
-    });
-    // [END authstatelistener]
-
-    document.getElementById('btnLogin').addEventListener('click', toggleSignIn, false);
-    document.getElementById('btnSignup').addEventListener('click', handleSignUp, false);
-    // document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
-    // document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
-}
-
-window.onload = function() {
-    initApp();
-};
-=======
->>>>>>> parent of e71e6cb... Update login.js
