a8bc515a5178b7be2d1c4d68e7eb3f2f3d5c5f57
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-03-12 11:39:09 +0700

    Update login.js

--- build/login/js/login.js
+++ build/login/js/login.js
@@ -28,13 +28,12 @@ function toggleSignIn() {
                 alert(errorMessage);
             }
             console.log(error);
-            document.getElementById('login-form').disabled = false;
-            upjjjjii
+            document.getElementById('btnLogin').disabled = false;
             // [END_EXCLUDE]
         });
         // [END authwithemail]
     }
-    document.getElementById('login-form').disabled = true;
+    document.getElementById('btnLogin').disabled = true;
 }
 
 /**
@@ -117,7 +116,7 @@ function initApp() {
     // [START authstatelistener]
     firebase.auth().onAuthStateChanged(function(user) {
         // [START_EXCLUDE silent]
-        document.getElementById('login-form').disabled = true;
+        document.getElementById('btnLogin').disabled = true;
         // [END_EXCLUDE]
         if (user) {
             // User is signed in.
@@ -129,9 +128,9 @@ function initApp() {
             var uid = user.uid;
             var providerData = user.providerData;
             // [START_EXCLUDE]
-            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+
+            // document.getElementById('btnLogin').textContent = 'Sign out';
+
             if (!emailVerified) {
                 document.getElementById('quickstart-verify-email').disabled = false;
             }
@@ -139,19 +138,19 @@ function initApp() {
         } else {
             // User is signed out.
             // [START_EXCLUDE]
-            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-            document.getElementById('quickstart-sign-in').textContent = 'Sign in';
-            document.getElementById('quickstart-account-details').textContent = 'null';
+
+            // document.getElementById('btnLogin').textContent = 'Sign in';
+
             // [END_EXCLUDE]
         }
         // [START_EXCLUDE silent]
-        document.getElementById('quickstart-sign-in').disabled = false;
+        document.getElementById('btnLogin').disabled = false;
         // [END_EXCLUDE]
     });
     // [END authstatelistener]
 
-    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-    document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
+    document.getElementById('btnLogin').addEventListener('click', toggleSignIn, false);
+    document.getElementById('btnSignup').addEventListener('click', handleSignUp, false);
     document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
     document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
 }
