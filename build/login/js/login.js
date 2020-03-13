5a2e23260830317354ad29fe5a8c0165d25eafce
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-03-13 17:05:44 +0700

    Update login.js

--- build/login/js/login.js
+++ build/login/js/login.js
@@ -1,7 +1,24 @@
 window.onload = function() {
 
+    const signupForm = this.document.getElementById("signup-form");
+    const signinForm = this.document.getElementById("signin-form");
     const btnSignin = document.getElementById("btnSignin");
     const btnSignup = document.getElementById("btnSignup");
+    const divSignup = document.getElementById("div-signup");
+    const divSignin = document.getElementById("div-signin");
+    // hide Sign up form
+    signupForm.style.display = "none";
+
+    //
+    divSignup.addEventListener('click', e => {
+        signupForm.style.display = "block";
+        signinForm.style.display = "none";
+    });
+    //
+    divSignin.addEventListener('click', e => {
+        signupForm.style.display = "none";
+        signinForm.style.display = "block";
+    });
     // Sign up
     btnSignup.addEventListener('click', e => {
         var emailNew = document.getElementById("email-signup").value;
@@ -22,19 +39,16 @@ window.onload = function() {
         });
 
     });
-    // Sign in
 
+    // Sign in
     btnSignin.addEventListener('click', e => {
-        if (firebase.auth().currentUser) {
-            // [START signout]
-            firebase.auth().signOut();
-            window.alert("User");
-            // [END signout]
-        } else {
-            var email = document.getElementById("email-signin").value;
-            var password = document.getElementById("password-signin").value;
-            window.alert(email + " " + password);
-            firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        var email = document.getElementById("email-signin").value;
+        var password = document.getElementById("password-signin").value;
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(function(response) {
+                window.alert("OK");
+            })
+            .catch(function(error) {
                 // Handle Errors here.
                 var errorCode = error.code;
                 var errorMessage = error.message;
@@ -42,34 +56,34 @@ window.onload = function() {
                 console.log(errorMessage);
             });
 
-        }
 
     });
 
+    // Sign out
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+    }).catch(function(error) {
+        // An error happened.
+    });
 
     // Add a realtime listener
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
             // var displayName = user.displayName;
-            // var email = user.email;
-            // window.location("https://ldr-pj.web.app")
-            // document.getElementById('nav-login').textContent = 'Profile';
-            // document.getElementById('nav-logout').classList.remove('hide');
+            var email = user.email;
             // var emailVerified = user.emailVerified;
             // var photoURL = user.photoURL;
             // var isAnonymous = user.isAnonymous;
             // var uid = user.uid;
             // var providerData = user.providerData;
             // ...
-            window.alert("Login")
-            console.log('logged in');
-
+            window.alert(email);
         } else {
-            console.log('not logged in');
-            // document.getElementById('nav-logout').classList.add('hide');
+            console.log("No logged in");
             // User is signed out.
             // ...
         }
     });
+
 }
