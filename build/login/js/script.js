3856fa5f107f64d6b0d8358cdf1ccf23313a8cb2
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-03-13 11:54:53 +0700

    Update script.js

--- build/login/js/script.js
+++ build/login/js/script.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-    $(".signup-form").hide();
-    // $(".signup").css("background", "none");
+// $(document).ready(function() {
+//     $(".signup-form").hide();
+//     // $(".signup").css("background", "none");
 
-    $(".login").click(function() {
-        $(".signup-form").hide();
-        $(".signin-form").show();
-        // $(".signup").css("background", "none");
-        // $(".login").css("background", "#fff");
-    });
+//     $(".login").click(function() {
+//         $(".signup-form").hide();
+//         $(".signin-form").show();
+//         // $(".signup").css("background", "none");
+//         // $(".login").css("background", "#fff");
+//     });
 
-    $(".signup").click(function() {
-        $(".signup-form").show();
-        $(".signin-form").hide();
-        // $(".signup").css("background", "#fff");
-        // $(".login").css("background", "none");
-    });
+//     $(".signup").click(function() {
+//         $(".signup-form").show();
+//         $(".signin-form").hide();
+//         // $(".signup").css("background", "#fff");
+//         // $(".login").css("background", "none");
+//     });
 
-    // $(".btn").click(function() {
-    //     $(".input").val("");
-    // });
-});
+//     // $(".btn").click(function() {
+//     //     $(".input").val("");
+//     // });
+// });
