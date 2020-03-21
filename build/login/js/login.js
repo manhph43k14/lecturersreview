facaeb77dbc409187127c4d89e8e863b843d29ed
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-03-21 12:44:43 +0700

    Update login.js

--- build/login/js/login.js
+++ build/login/js/login.js
@@ -1,5 +1,84 @@
-window.onload = function() {
+// Sign up
+function handleSignUp() {
+    var emailNew = document.getElementById("email-signup").value;
+    var passwordNew = document.getElementById("password-signup").value;
+    if (emailNew.length < 6) {
+        window.alert('Please enter an email address.');
+        return;
+    } else
+    if (passwordNew.length < 6) {
+        window.alert('The password is too weak.');
+        return;
+    } else {
+        window.alert('Create user successfull!');
+        return;
+    }
 
+    // Create user with email and pass.
+    // [START createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(emailNew, passwordNew).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(errorMessage);
+    });
+
+};
+// Sign in
+function toggleSignIn() {
+    if (firebase.auth().currentUser) {
+        // [START signout]
+        window.location.assign("../index.html");
+        // [END signout]
+    } else {
+        var userEmail = document.getElementById("email-signin").value;
+        var userPassword = document.getElementById("password-signin").value;
+        if (userEmail.length < 4) {
+            alert('Please enter an email address.');
+            return;
+        }
+        if (userEmail.length < 4) {
+            alert('Please enter a password.');
+            return;
+        }
+        // Sign in with email and pass.
+        // [START authwithemail]
+        firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
+            .then(function(response) {
+                //
+            })
+            .catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // [START_EXCLUDE]
+                if (errorCode === 'auth/wrong-password') {
+                    alert('Wrong password.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(errorMessage);
+            });
+
+    }
+};
+
+function signout() {
+    // Sign out
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+    }).catch(function(error) {
+        // An error happened.
+    });
+}
+
+function initApp() {
     const signupForm = this.document.getElementById("signup-form");
     const signinForm = this.document.getElementById("signin-form");
     const btnSignin = document.getElementById("btnSignin");
@@ -19,85 +98,6 @@ window.onload = function() {
         signupForm.style.display = "none";
         signinForm.style.display = "block";
     });
-    // Sign up
-    btnSignup.addEventListener('click', e => {
-        var emailNew = document.getElementById("email-signup").value;
-        var passwordNew = document.getElementById("password-signup").value;
-        if (emailNew.length < 6) {
-            window.alert('Please enter an email address.');
-            return;
-        } else
-        if (passwordNew.length < 6) {
-            window.alert('The password is too weak.');
-            return;
-        } else {
-            window.alert('Create user successfull!');
-            return;
-        }
-
-        // Create user with email and pass.
-        // [START createwithemail]
-        firebase.auth().createUserWithEmailAndPassword(emailNew, passwordNew).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // [START_EXCLUDE]
-            if (errorCode == 'auth/weak-password') {
-                alert('The password is too weak.');
-            } else {
-                alert(errorMessage);
-            }
-            console.log(errorMessage);
-        });
-
-    });
-
-    // Sign in
-    btnSignin.addEventListener('click', e => {
-        if (firebase.auth().currentUser) {
-            // [START signout]
-            firebase.auth().signOut();
-            // [END signout]
-        } else {
-            var userEmail = document.getElementById("email-signin").value;
-            var userPassword = document.getElementById("password-signin").value;
-            if (userEmail.length < 4) {
-                alert('Please enter an email address.');
-                return;
-            }
-            if (userEmail.length < 4) {
-                alert('Please enter a password.');
-                return;
-            }
-            // Sign in with email and pass.
-            // [START authwithemail]
-            firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
-                .then(function(response) {
-                    window.location.assign("../index.html");
-                })
-                .catch(function(error) {
-                    // Handle Errors here.
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    // [START_EXCLUDE]
-                    if (errorCode === 'auth/wrong-password') {
-                        alert('Wrong password.');
-                    } else {
-                        alert(errorMessage);
-                    }
-                    console.log(errorMessage);
-                });
-
-        }
-    });
-
-    // Sign out
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-    }).catch(function(error) {
-        // An error happened.
-    });
-
     // Add a realtime listener
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
@@ -117,5 +117,10 @@ window.onload = function() {
             // ...
         }
     });
+    btnSignin.addEventListener('click', toggleSignIn, false);
+    btnSignup.addEventListener('click', handleSignUp, false);
+}
 
+window.onload = function() {
+    initApp();
 }
