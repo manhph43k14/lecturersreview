2a3fcda5ef3b25a4cf0f465193f222f24bd71abb
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-05-11 08:51:38 +0700

    update resNav

--- duelecturersreview/src/Nav/nav.js
+++ duelecturersreview/src/Nav/nav.js
@@ -43,20 +43,23 @@ class Nav extends Component {
         
         return (
             <div className="navbar navbar-default navbar-fixed-top">
-                <div className="container">
-                    <div className="navbar-header">
+                <div className="container col-md-10" >
+                    <div className="navbar-header ">
                         <NavLink className="navbar-brand" to="/">
-                            <span><img className="logo" src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.0-9/305161_228819400508433_809369959_n.jpg?_nc_cat=111&_nc_sid=7aed08&_nc_ohc=VhtM4uGky5QAX_EsF-z&_nc_ht=scontent.fdad2-1.fna&oh=32577063fd6104c22502cefc12691421&oe=5ED99679" alt="logo" height={40} width={40} /></span>
-                            <span>DUE LECTURERS REVIEW</span>
+                            <span><img className="logo" src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.0-9/305161_228819400508433_809369959_n.jpg?_nc_cat=111&_nc_sid=7aed08&_nc_ohc=VhtM4uGky5QAX_EsF-z&_nc_ht=scontent.fdad2-1.fna&oh=32577063fd6104c22502cefc12691421&oe=5ED99679" alt="logo" height={40} width={40} /></span>    
                         </NavLink>
-                    </div>
-                    <div id="navbar" className="navbar-collapse collapse">
                         <ul className="nav navbar-nav navbar-right">
                             <li><NavLink to="/">Home</NavLink></li>
                             <li><NavLink to="/find">Find</NavLink></li>
                             <li id="nav-login"><NavLink to="/login">Log in</NavLink></li>
                             <li id="nav-logout" className="hide"><NavLink to="/" onClick={this.logOut.bind(this, "logOut()")}>Logout</NavLink></li>
                         </ul>
+                    </div>
+                    <div id="navbar" className="navbar-collapse collapse navbar-right">
+                        <NavLink className="navbar-brand" to="/">
+                            <span>DUE LECTURERS REVIEW</span>
+                        </NavLink>
+                        
                     </div>
                 </div>
             </div>
